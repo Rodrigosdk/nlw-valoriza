017557854cc90b1de5f0f1dcab600f2aa5780911
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-06-22 18:04:22 -0300

    Created user controller

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,15 @@
+import {Request,Response} from 'express'
+import {CreateUserServices} from '../services/CreateUserServices'
+
+class CreateUserController{
+    async handle(request:Request, response:Response){
+        const {name, email, admin} = request.body
+
+        const createUserServices = new CreateUserServices();
+        const user = await createUserServices.execute({name, email, admin})
+        
+        return response.json(user);
+    }
+}
+
+export {CreateUserController}
