53d41a4f2c6676940cdb7d1dbac175596710aaf7
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-07-22 22:38:48 -0300

    resolution of erro and refactod code

--- src/middlewares/Erros.ts
+++ src/middlewares/Erros.ts
@@ -1,10 +1,15 @@
-import {NextFunction, Request,Response} from 'express'
+import { NextFunction, Request, Response } from 'express'
 
-export function error(request:Request, response:Response,error:Error, next:NextFunction): Response{
-    if (error instanceof Error){
-        return response.status(400).json({"error": error.message})
+export function error(
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction
+): Response {
+    if (error instanceof Error) {
+        return response.status(400).json({ "error": error.message })
     }
-    
+
     return response.status(500).json({
         status: "error",
         message: "Internal Server Error"
