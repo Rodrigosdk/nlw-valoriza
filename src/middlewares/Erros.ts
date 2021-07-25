9d23cc99b0ec1de660e5b95c38944c4b4ea20cc3
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-07-24 21:39:44 -0300

    added new type for better understanding of middlewares

--- src/middlewares/Erros.ts
+++ src/middlewares/Erros.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
+import { error } from '../types/middlewares/error'
 
 export function error(
     error: Error,
     request: Request,
     response: Response,
     next: NextFunction
-): Response {
+): Response<error> {
     if (error instanceof Error) {
-        return response.status(400).json({ "error": error.message })
+        return response.status(400).json({
+            status: "error",
+            message: error.message
+        })
     }
 
     return response.status(500).json({
