0ffa29015f1367d30b73de7e0665220bf928b40f
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-08-02 22:46:50 -0300

    create middlewares ensure admin

--- src/routes/TagRouter.ts
+++ src/routes/TagRouter.ts
@@ -1,10 +1,11 @@
 import { CreateTagController } from '../controllers/CreateTagController'
 import { Application } from 'express'
+import { ensureAdmin } from '../middlewares/ensureAdmin'
 
 export class TagRouter {
     private createTagController: CreateTagController = new CreateTagController()
 
     public init(app: Application): void {
-        app.post('/tags', this.createTagController.handle)
+        app.post('/tags',ensureAdmin, this.createTagController.handle)
     }
 }
